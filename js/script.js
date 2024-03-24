1ee8bd21c42d6c45e7f313f256223720ae9550bd
EllisOllier <ellisollier1@gmail.com>
2024-03-24 14:05:23 +0000

    Improved code quality

--- js/script.js
+++ js/script.js
@@ -1,22 +1,28 @@
+const DEFAULT_GRID_SIZE = 16;
+const CONTAINER = document.getElementById("canvas-container");
+const CONTAINER_HEIGHT = CONTAINER.clientHeight;
+const CONTAINER_WIDTH = CONTAINER.clientWidth;
+
+createGrid(DEFAULT_GRID_SIZE);
 document.addEventListener('mouseover', function(e){
     if(e.target.id === "pixel"){
         let currentBrushColor = document.getElementById("brush-color");
         e.target.style.backgroundColor = currentBrushColor.value;
     }
 });
-const container = document.getElementById("canvas-container");
+
 function createGrid(gridSize) {
     clearGrid();
     for(j = 0; j < gridSize; j++){
         const row = document.createElement("div");
         row.className = "row";
-        container.appendChild(row);
+        CONTAINER.appendChild(row);
 
         for(let i = 0; i < gridSize; i++){
             const col = document.createElement("div");
             col.style.flexGrow = 1;
-            col.style.height = `${(400 / gridSize)}px`
-            col.style.width = `${(400 / gridSize)}px`
+            col.style.height = `${(CONTAINER_HEIGHT / gridSize)}px`
+            col.style.width = `${(CONTAINER_WIDTH / gridSize)}px`
             col.id = "pixel";
             col.style.backgroundColor = document.getElementById("background-color").value;
             row.appendChild(col);
